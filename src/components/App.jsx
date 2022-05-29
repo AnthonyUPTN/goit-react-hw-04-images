b80206dae67c1dd78cb4ea8c12889df392d5242e
AnthonyUPTN <tagirovantn@gmail.com>
2022-05-29 11:11:33 +0300

    вщту

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { RotatingLines } from 'react-loader-spinner';
 
 import Searchbar from './Searchbar';
@@ -9,100 +9,206 @@ import Modal from 'shared/components/Modal';
 
 import { getImages } from '../shared/services/fetch';
 
-class App extends Component {
-  state = {
-    q: '',
-    page: 1,
+const App = () => {
+  const [q, setQ] = useState('');
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState({
     images: [],
     loading: false,
     error: null,
     total: 0,
+  });
+  const [modal, setModal] = useState({
     modalBody: {},
     isModalOpen: false,
-  };
+  });
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { q, page } = this.state;
-    if (q !== prevState.q || page > prevState.page) {
-      this.setState({
+  useEffect(() => {
+    if (q === '') {
+      return;
+    }
+
+    setData(prevState => {
+      return {
+        ...prevState,
         loading: true,
         error: false,
-      });
+      };
+    });
 
+    const data = async (q, page) => {
       try {
         const data = await getImages(q, page);
-        this.setState(prevState => {
+        setData(prevState => {
           return {
+            ...prevState,
             images: [...prevState.images, ...data.hits],
             loading: false,
             total: data.total,
           };
         });
       } catch (error) {
-        this.setState({
-          loading: false,
-          error: error.message,
+        setData(prevState => {
+          return {
+            ...prevState,
+            loading: false,
+            error: error.message,
+          };
         });
       }
-    }
-  }
+    };
+    data(q, page);
+  }, [q, page]);
 
-  onSearch = ({ q }) => {
-    this.setState({
-      q,
-      page: 1,
-      images: [],
-    });
-  };
-
-  loadMore = () => {
-    this.setState(({ page }) => {
+  const onSearch = ({ q }) => {
+    setQ(q);
+    setPage(1);
+    setData(prevState => {
       return {
-        page: page + 1,
+        ...prevState,
+        q,
+        page: 1,
+        images: [],
       };
     });
   };
 
-  showModal = modalBody => {
-    this.setState({
+  const loadMore = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  const showModal = modalBody => {
+    setModal({
       isModalOpen: true,
       modalBody,
     });
   };
 
-  closeModal = () => {
-    this.setState({
-      isModalOpen: false,
+  const closeModal = () => {
+    setModal(prevState => {
+      return { ...prevState, isModalOpen: false };
     });
   };
 
-  render() {
-    const { images, loading, total, q, isModalOpen, modalBody } = this.state;
-    const { onSearch, loadMore, showModal, closeModal } = this;
+  const { modalBody, isModalOpen } = modal;
+  const { images, loading, total } = data;
 
-    return (
-      <>
-        <Searchbar onSubmit={onSearch} />
-        {!loading && !images.length && Boolean(q.length) && (
-          <p>Требуется правильный ввод!</p>
-        )}
-        {loading && <RotatingLines width="100" />}
-        <ImageGallery data={images} onClick={showModal} />
-        {!loading && Boolean(images.length) && images.length < total && (
-          <Button onClick={loadMore} />
-        )}
-        {isModalOpen && (
-          <Modal onClose={closeModal}>
-            <img
-              src={modalBody.largeImageURL}
-              alt={modalBody.tag}
-              width="800"
-            />
-          </Modal>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Searchbar onSubmit={onSearch} />
+      {!loading && !images.length && Boolean(q.length) && (
+        <p>Требуется правильный ввод!</p>
+      )}
+      {loading && <RotatingLines width="100" />}
+      <ImageGallery data={images} onClick={showModal} />
+      {!loading && Boolean(images.length) && images.length < total && (
+        <Button onClick={loadMore} />
+      )}
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <img src={modalBody.largeImageURL} alt={modalBody.tag} width="800" />
+        </Modal>
+      )}
+    </>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     q: '',
+//     page: 1,
+//     images: [],
+//     loading: false,
+//     error: null,
+//     total: 0,
+//     modalBody: {},
+//     isModalOpen: false,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     const { q, page } = this.state;
+//     if (q !== prevState.q || page > prevState.page) {
+//       this.setState({
+//         loading: true,
+//         error: false,
+//       });
+
+//       try {
+//         const data = await getImages(q, page);
+//         this.setState(prevState => {
+//           return {
+//             images: [...prevState.images, ...data.hits],
+//             loading: false,
+//             total: data.total,
+//           };
+//         });
+//       } catch (error) {
+//         this.setState({
+//           loading: false,
+//           error: error.message,
+//         });
+//       }
+//     }
+//   }
+
+//   onSearch = ({ q }) => {
+//     this.setState({
+//       q,
+//       page: 1,
+//       images: [],
+//     });
+//   };
+
+//   loadMore = () => {
+//     this.setState(({ page }) => {
+//       return {
+//         page: page + 1,
+//       };
+//     });
+//   };
+
+//   showModal = modalBody => {
+//     this.setState({
+//       isModalOpen: true,
+//       modalBody,
+//     });
+//   };
+
+//   closeModal = () => {
+//     this.setState({
+//       isModalOpen: false,
+//     });
+//   };
+
+//   render() {
+//     const { images, loading, total, q, isModalOpen, modalBody } = this.state;
+//     const { onSearch, loadMore, showModal, closeModal } = this;
+
+//     return (
+//       <>
+//         <Searchbar onSubmit={onSearch} />
+//         {!loading && !images.length && Boolean(q.length) && (
+//           <p>Требуется правильный ввод!</p>
+//         )}
+//         {loading && <RotatingLines width="100" />}
+//         <ImageGallery data={images} onClick={showModal} />
+//         {!loading && Boolean(images.length) && images.length < total && (
+//           <Button onClick={loadMore} />
+//         )}
+//         {isModalOpen && (
+//           <Modal onClose={closeModal}>
+//             <img
+//               src={modalBody.largeImageURL}
+//               alt={modalBody.tag}
+//               width="800"
+//             />
+//           </Modal>
+//         )}
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,59 +1,99 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import s from './searchbar.module.css';
 
-class Searchbar extends Component {
-  state = {
-    q: '',
+const Searchbar = ({ onSubmit }) => {
+  const [q, setQ] = useState('');
+
+  const handleChange = ({ target }) => {
+    setQ(target.value);
   };
 
-  handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit({ ...this.state });
-    this.reset();
+    onSubmit({ q });
+    reset();
   };
 
-  reset() {
-    this.setState({
-      q: '',
-    });
-  }
+  const reset = () => {
+    setQ('');
+  };
 
-  render() {
-    const { handleChange, handleSubmit } = this;
-    return (
-      <header className={s.searchbar}>
-        <form className={s.form} onSubmit={handleSubmit}>
-          <button type="submit" className={s.button}>
-            <span className={s.buttonLabel}>Search</span>
-          </button>
+  return (
+    <header className={s.searchbar}>
+      <form className={s.form} onSubmit={handleSubmit}>
+        <button type="submit" className={s.button}>
+          <span className={s.buttonLabel}>Search</span>
+        </button>
 
-          <input
-            onChange={handleChange}
-            className={s.input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            name="q"
-            value={this.state.q}
+        <input
+          onChange={handleChange}
+          className={s.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="q"
+          value={q}
           />
-        </form>
-      </header>
-    );
-  }
-}
+      </form>
+    </header>
+  );
+};
 
 export default Searchbar;
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+// class Searchbar extends Component {
+//   state = {
+//     q: '',
+//   };
+
+//   handleChange = ({ target }) => {
+//     const { name, value } = target;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     this.props.onSubmit({ ...this.state });
+//     this.reset();
+//   };
+
+//   reset() {
+//     this.setState({
+//       q: '',
+//     });
+//   }
+
+//   render() {
+//     const { handleChange, handleSubmit } = this;
+//     return (
+//       <header className={s.searchbar}>
+//         <form className={s.form} onSubmit={handleSubmit}>
+//           <button type="submit" className={s.button}>
+//             <span className={s.buttonLabel}>Search</span>
+//           </button>
+
+//           <input
+//             onChange={handleChange}
+//             className={s.input}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             name="q"
+//             value={this.state.q}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
